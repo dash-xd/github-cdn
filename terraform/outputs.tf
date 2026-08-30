d1d6e26286dc57aa667faaa063d42ac22b205999
dev@dashxd <125025267+dash-xd@users.noreply.github.com>
2026-08-29 23:20:40 -0700

    Expose composed deployment outputs

---
+++ terraform/outputs.tf
@@ -0,0 +1,19 @@
+output "function_name" {
+  value = google_cloudfunctions2_function.github_cdn.name
+}
+
+output "function_url" {
+  value = google_cloudfunctions2_function.github_cdn.url
+}
+
+output "function_uri" {
+  value = google_cloudfunctions2_function.github_cdn.service_config[0].uri
+}
+
+output "runtime_service_account" {
+  value = google_service_account.runtime.email
+}
+
+output "source_bucket" {
+  value = google_storage_bucket.source.name
+}
