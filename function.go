607b585369ab81f07308226480822d256afc4cfc
dev@dashxd <125025267+dash-xd@users.noreply.github.com>
2026-08-29 23:45:48 -0700

    Use gospace-minimal style Cloud Functions entry point

--- function.go
+++ function.go
@@ -1,16 +1,12 @@
-package githubcdn
+// Package function exposes the HTTP entry point discovered by the Go
+// Cloud Functions Gen2 buildpack. The router itself is ordinary net/http;
+// no local Functions Framework dependency is required.
+package function
 
 import (
 	"net/http"
 
-	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
 	"github.com/dash-xd/github-cdn/app"
 )
 
-func init() {
-	functions.HTTP("Main", Main)
-}
-
-func Main(w http.ResponseWriter, r *http.Request) {
-	app.Handler().ServeHTTP(w, r)
-}
+var Main func(http.ResponseWriter, *http.Request) = app.Handler().ServeHTTP
