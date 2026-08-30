1ea4310fe07cb799885312ecf68511bbb69820a2
dev@dashxd <125025267+dash-xd@users.noreply.github.com>
2026-08-29 22:12:36 -0700

    Add private Cloud Run function Terraform deployment

---
+++ terraform/versions.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.7"
+    }
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 7.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.7"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
